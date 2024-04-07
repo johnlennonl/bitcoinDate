80223e1594ea667506f94e3e5e2eeaa669a093eb
johnlennonl <jojopow2410@gmail.com>
2024-04-06 20:34:45 -0400

    M3 5365464

---
+++ index.js
@@ -0,0 +1,12 @@
+const dollarContainer = document.getElementById('dollar');
+const usdAmount = document.getElementById('usd-amount');
+
+fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+    .then(response => response.json())
+    .then(data => displayData(data));
+
+const displayData = data => {
+    const usd = data.bpi.USD.rate_float;
+    usdAmount.textContent = `$${usd} USD`;
+    
+    } 
